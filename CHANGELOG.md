d69df67e5cbf616d987dac579d4aa85f8541ee74
Jelle Siderius <jelle@hypershop.nl>
2025-05-21 10:50:49 +0200

    Release 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log for Sidworks SearchResults plugin
 All notable changes to this module will be documented in this file.
 
+## 1.0.1 [21-05-2025]
+### Added
+- String length configuration
+
 ## 1.0.0 [20-05-2025]
 ### Added
 - Initial release
--- src/Subscriber/SearchSubscriber.php
+++ src/Subscriber/SearchSubscriber.php
@@ -9,11 +9,13 @@ use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Storefront\Page\Search\SearchPageLoadedEvent;
 use Shopware\Storefront\Page\Suggest\SuggestPageLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class SearchSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private readonly EntityRepository $searchResultsRepository
+        private readonly EntityRepository $searchResultsRepository,
+        private readonly SystemConfigService $systemConfigService
     ) {}
 
     public static function getSubscribedEvents(): array
@@ -29,15 +31,17 @@ class SearchSubscriber implements EventSubscriberInterface
         $request = $event->getRequest();
         $term = trim((string) $request->query->get('search', ''));
 
-        if ($term === '' || strlen($term) > 255) {
+        $term = strip_tags($term);
+
+        $minLength = (int) $this->systemConfigService->get('SidworksSearchResults.config.minStringLength') ?? 2;
+        $maxLength = (int) $this->systemConfigService->get('SidworksSearchResults.config.maxStringLength') ?? 255;
+
+        if ($term === '' || strlen($term) < $minLength || strlen($term) > $maxLength) {
             return;
         }
 
-        $term = strip_tags($term);
-
         $context = $event->getContext();
         $salesChannelId = $event->getSalesChannelContext()->getSalesChannel()->getId();
-
 
         $criteria = new Criteria();
         $criteria->addFilter(new EqualsFilter('searchTerm', $term));
